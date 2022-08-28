629cf21909654265b9be2df8a40c4206ac453452
IverSutizal <iversutizal@gmail.com>
2022-08-28 00:06:14 -0500

    perform insertion sort on a linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,34 +1,46 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - perform insertion sort on a linked list
- * @list: head node of the linked list
+ * swap_nds - Swap two nodes in a double linked list.
+ * @list: Pointer to double linked list head
+ * @n1: Pointer to first node.
+ * @n2: Pointer to second node.
+ */
+void swap_nds(listint_t **list, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*list = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending
+ * order using the Insertion sort algorithm
+ * @list: Pointer to double linked list head
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *swap, *prv;
+	listint_t *node = NULL, *prev = NULL, *tmp = NULL;
 
-	if (!list || !*list)
+	if (!list || !(*list) || !(*list)->next)
 		return;
-	current = *list;
-	while ((current = current->next))
-	{
-		swap = current;
-		{
-		while (swap->prev && swap->n < swap->prev->n)
-		{
-			prv = swap->prev;
-			if (swap->next)
-				swap->next->prev = prv;
-			if (prv->prev)
-				prv->prev->next = swap;
-			else
-			*list = swap;
-		prv->next = swap->next;
-		swap->prev = prv->prev;
-		swap->next = prv;
-		prv->prev = swap;
 
-			print_list(*list);
+	for (node = (*list)->next; node; node = tmp)
+	{
+		tmp = node->next;
+		prev = node->prev;
+		while (prev && node->n < prev->n)
+		{
+			swap_nds(list, &prev, node);
+			print_list((const listint_t *)*list);
 		}
 	}
 }
